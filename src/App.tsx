b28d33157e96fb8f06721ed0385cf590062042d3
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-04-05 13:50:26 +0100

    add wrapper and styles component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 //compnents//
+import Item from './Item/Item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -30,11 +31,30 @@ const App = () => {
     getProducts
   );
   console.log(data);
+
+const getTotalItems = () => null;
+
+const handleAddToCart = (clickedItem: CartItemType) => null;
+
+const handleRemoveFromCart = () => null;
+
+if (isLoading) return <LinearProgress />;
+if (error) return <div>Something went wrong ...</div>;
+
+
+
+
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4} >
+          <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+        </Grid>
+    </Wrapper>
   );
-}
+};
 
 export default App;
