dff0f85a48ac0fdb0b88ebe909d39c52569924a9
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-04-06 12:20:33 +0100

    add final add and remove to cart functions

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -11,6 +11,8 @@ type Props = {
 };
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) =>
+  items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
 
   return (
     <Wrapper>
@@ -20,10 +22,11 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
           <CartItem 
           key={item.id}
           item={item}
-          // addToCart={addToCart}
+          addToCart={addToCart}
           removeFromCart={removeFromCart}
           />
       ))}
+      <h2>Total: {calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   )
 }
--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -6,10 +6,40 @@ import { Wrapper } from './CartItem.styles';
 
 type Props = {
   item: CartItemType;
-  // addToCart: (clickedItem: CartItem) => void;
+  addToCart: (clickedItem: CartItemType) => void;
   removeFromCart: (id: number) => void;
 }
 
-const CartItem: React.FC<Props> = () => <div>Cart Item</div>;
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => (
+  <Wrapper>
+    <div>
+      <h3>{item.title}</h3>
+      <div className='information'>
+        <p>Price: {item.price}</p>
+        <p>Total: {(item.amount * item.price).toFixed(2)}</p>
+      </div>
+      <div className='buttons'>
+        <Button 
+        size='small'
+        disableElevation
+        variant='contained'
+        onClick={() => removeFromCart(item.id)}
+        >
+          -
+        </Button>
+        <p>Item amount</p>
+        <Button 
+        size='small'
+        disableElevation
+        variant='contained'
+        onClick={() => addToCart(item)}
+        >
+          +
+        </Button>
+      </div>
+    </div>
+    <img src={item.image} alt={item.title}/>
+  </Wrapper>
+)
 
 export default CartItem;
